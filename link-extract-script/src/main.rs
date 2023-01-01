c9795c81135db7abbe0a7e1c63f22b25e3753231
hardikthecoder <85208662+hardiktheprogrammer@users.noreply.github.com>
2023-01-01 06:18:56 +0530

    fixed Bug

--- link-extract-script/src/main.rs
+++ link-extract-script/src/main.rs
@@ -1,12 +1,18 @@
 use error_chain::error_chain;
-use select::document::Document;
+use select::document::Document; //document selector on pages
 use select::predicate::Name;
 
 error_chain! {
-    foreign_links{
-        ReqError(reqwest::error);
+    foreign_chain!{
+        ReqError(reqwest::Error); //give a error name
+        IoError(std::io::Error);
 
 
 
     }
 }
+#[tokio::main]
+
+async fn main() -> Result<()> {
+    let res = reqwest 
+} //asyc request
